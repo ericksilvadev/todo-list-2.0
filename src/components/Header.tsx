41442962cd674242bae7c7d9f3faa36b4a29e4c0
ericksilvadev <erick.silva6653@gmail.com>
2021-11-09 10:43:59 -0300

    mobile support

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 import moonIcon from '../images/icon-moon.svg';
 import sunIcon from '../images/icon-sun.svg';
@@ -6,10 +6,30 @@ import sunIcon from '../images/icon-sun.svg';
 const Header = () => {
   const [theme, setTheme] = useState(true);
 
+  useEffect(() => {
+    const storageTheme = localStorage.getItem('theme');
+
+    if (storageTheme) {
+      if (JSON.parse(storageTheme) === 'light') {
+        document.getElementById('body')?.setAttribute('theme', 'light');
+        setTheme(true);
+      } else {
+        document.getElementById('body')?.setAttribute('theme', 'dark');
+        setTheme(false);
+      }
+    }
+  }, []);
+
   const handleDark = () => {
     document.getElementById('body')?.setAttribute('theme', theme ? 'dark' : 'light');
     setTheme(!theme);
+    if (!theme) {
+      localStorage.setItem('theme', JSON.stringify('light'));
+    } else {
+      localStorage.setItem('theme', JSON.stringify('dark'));
+    }
   };
+
   return (
     <header className={theme ? '' : 'dark'}>
       <div className="header-content">
